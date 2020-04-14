67bcda0658d90f1b36f21b5dfd9d44e87cd97277
Anmicius <allenlin@disroot.org>
2020-04-14 17:36:38 +0800

    add comment

--- process.py
+++ process.py
@@ -1,23 +1,37 @@
-#!/usr/bin/python
-
 import subprocess
 import sys
 
-#ssh command define
+
 def SSH(COMMAND):
+    """Example function with PEP 484 type annotations.
+
+    Args:
+        COMMAND (string): The first parameter.
+
+    Returns:
+        The return value. True for success, False otherwise.
+
+    """
+
     # Host
     HOST = "allenlin3024@cnmc.tw"
+
     # Process
     ssh = subprocess.Popen(["ssh", "%s" % HOST, COMMAND],
                            shell=False,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
+
     result = ssh.stdout.readlines()
-    #
+
+    # if error
     if result == []:
         error = ssh.stderr.readlines()
-        print (sys.stderr, "ERROR: %s" % error)
+        print(sys.stderr, "ERROR: %s" % error)
+    # if success
     else:
         print(result)
-#where am i
+
+
+# where am i
 SSH("pwd")
