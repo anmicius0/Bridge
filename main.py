0dced137a2494623773ec0e358b9ad98f89e3ef7
Anmicius <allenlin@disroot.org>
2020-04-15 14:15:13 +0800

    update main.py: handle new format of respose from request.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ def main():
     post_r = add_post("美好的標題", "精彩的內容")
 
     if post_r["status"] == "error":
-        return "add_post error"
+        return f"add_post error on post {post_r{'title'}}"
 
     # try acf
     acf_r = add_acf(id=post_r["id"], genre="學生", sub_genre_student="段考", repeater_link=[
         {"description": "firefox", "url": "https://firefox.com"}, {"description": "google", "url": "https://google.com"}])
 
     if acf_r["status"] == "error":
-        return acf_r
+        return f"add_acf error on {acf_r['id']}"
 
-    return "success"
+    return f"success on post {post_r['title']}"
